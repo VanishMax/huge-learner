3a77098d13b188513bc85fd5a2d3df95a9335621
VanishMax <maxkors2014@gmail.com>
2020-10-27 23:14:22 +0300

    Add snowpack-based project with configured eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: "airbnb-typescript",
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    // ES options
+    semi: [2, 'always'],
+    'no-console': ['warn', { 'allow': ['warn', 'error'] }],
+    'space-before-function-paren': 0,
+    'jsx-quotes': 0,
+    'no-class-assign': 0,
+    'brace-style': 0,
+    'object-curly-spacing': 0,
+    'no-return-assign': 0,
+    'no-trailing-spaces': 0,
+    'no-param-reassign': 0,
+    'comma-dangle': 0,
+    curly: [0, 'multi'],
+    'import/prefer-default-export': 0,
+
+    // Typescript options
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'comma',
+        requireLast: true,
+      },
+    }],
+  },
+};
---
+++ frontend/snowpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    ['@snowpack/plugin-run-script', {
+      'cmd': "eslint 'src/**/*.{js,jsx,ts,tsx}' --fix",
+      'output': 'stream'
+    }]
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    port: 3000,
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
