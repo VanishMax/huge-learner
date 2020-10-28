f9e6f0e9fc912a09cb82b5190c082b3f077d38f8
VanishMax <maxkors2014@gmail.com>
2020-10-28 11:51:45 +0300

    Add sentry bug tracer

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,17 @@
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+
+Sentry.init({
+  dsn: 'https://c24719dfd7314f37bb41c5f614f4f22a@o380629.ingest.sentry.io/5495860',
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +20,6 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/#hot-module-replacement
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
